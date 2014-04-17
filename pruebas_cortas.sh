856a47da4a3506b8ce754e57b29f38c3e3545bb3
Dani Fernandez <daniel.fx.90@gmail.com>
2014-04-17 20:40:40 -0300

    pruebas

---
+++ pruebas_cortas.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+prueba="Archivo vacio"
+touch /tmp/zero.txt
+./tp0 -a encode -i /tmp/zero.txt -o /tmp/zero.txt.b16
+longitud=`ls -la /tmp/zero.txt.b16 | awk '{print $5}'`
+if [[ $longitud -eq 0 ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
+rm -f /tmp/zero.txt /tmp/zero.txt.b16
+
+prueba="Codificacion de 'M' por entrada estandar"
+hexa=`echo -n M | ./tp0`
+if [[ "$hexa" == "4D" ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
+
+prueba="Codificacion de 'Ma' por entrada estandar"
+hexa=`echo -n Ma | ./tp0`
+if [[ "$hexa" == "4D61" ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
+
+prueba="Codificacion de 'Man' por entrada estandar"
+hexa=`echo -n Man | ./tp0`
+if [[ "$hexa" == "4D616E" ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
+
+prueba="Codificacion y decodificacion de 'Man' por entrada estandar"
+mensaje=`echo -n Man | ./tp0 | ./tp0 -a decode`
+if [[ "$mensaje" == "Man" ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
+
+prueba="Verificacion bit a bit de codificacion y decodificacion de xyz\n"
+esperado="0000000   x   y   z  \n
+0000004"
+resultado=`echo xyz | ./tp0 | ./tp0 -a decode | od -t c`
+if [[ "$resultado" == "$esperado" ]] ; then echo "ok: $prueba"; else echo "ERROR: $prueba" ; fi
