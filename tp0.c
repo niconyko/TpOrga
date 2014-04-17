b4ee4f5c3befc7f2afd373cb4d1561fd5c11db0a
Dani Fernandez <daniel.fx.90@gmail.com>
2014-04-17 19:47:10 -0300

    refactor mas string local estatico en encoder

--- tp0.c
+++ tp0.c
@@ -5,17 +5,9 @@
 #include <stdbool.h>
 
 
-
 static bool encoderActivo = true;
-static bool decoderActivo = false;
-
-FILE* finput = NULL;
-FILE* foutput = NULL;
-
-
 
 static char vecHexa [] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-static char valorHexa[2];
 
 static struct option long_options[] = {
 	 {"version", no_argument, 0, 'v'},
@@ -27,8 +19,7 @@ static struct option long_options[] = {
 };
 
 
-char* encoder( int numInt){
-	
+void encoder (char* valorHexa, unsigned int numInt) {
 	int highNible = numInt & 0xf0;
 	int lowNible = numInt & 0x0f;
 	int primerNum = highNible >> 4;
@@ -36,19 +27,17 @@ char* encoder( int numInt){
 	
 	valorHexa[0] = vecHexa[primerNum];
 	valorHexa[1] = vecHexa[segundoNum];
-		
-	return valorHexa;
+	valorHexa[2] = '\0';
 }
 
-
 int correrReferencia ( int numInt ){
-	if( numInt > 47 && numInt < 58)
+	if ( numInt > 47 && numInt < 58)
 		return numInt - 48 ;
 	
-	else if( numInt > 64 && numInt < 71)
+	else if ( numInt > 64 && numInt < 71)
 		return numInt - 55 ;
 	
-	else if( numInt > 96 && numInt < 103)
+	else if ( numInt > 96 && numInt < 103)
 		return numInt - 87 ;
 	else {
 		fprintf(stderr,"Contiene caracteres que no pertenecen al codigo Hexa\n");
@@ -57,10 +46,9 @@ int correrReferencia ( int numInt ){
 	
 }
 
-char decoder ( int numPri, int numSeg){
-	
-	int valor1 = correrReferencia( numPri );
-	int valor2 = correrReferencia( numSeg );
+char decoder (int numPri, int numSeg) {
+	int valor1 = correrReferencia (numPri);
+	int valor2 = correrReferencia (numSeg);
 	
 	int highNible = valor1 << 4;
 	highNible =  highNible & 0xf0;
@@ -69,104 +57,56 @@ char decoder ( int numPri, int numSeg){
 	char caracter = highNible | valor2;
 	
 	return caracter;
-	
-
 }
 
-void procesarArchivos(FILE* finput, FILE* foutput){
-	
-	char* string;
-	char c;
+int leer (FILE* finput) {
+	int caracter = fgetc (finput);
+	if (ferror(finput)) {
+		fprintf(stderr,"Error al leer el archivo de entrada\n");
+		exit(1);
+	}
+	return caracter;
+}
+
+void escribir_error () {
+	fprintf(stderr,"Error al escribir el archivo de salida\n");
+	exit(1);
+}
+
+void procesarArchivos (FILE* finput, FILE* foutput) {
+	int c;
 	int caracter2;
-	int caracter = fgetc(finput);
+	int caracter = leer(finput);
 	
-	while (caracter != EOF ){		
-		if(encoderActivo){
-			string = encoder(caracter);			
-			if(foutput != NULL){
-				if( fputs(string , foutput) == EOF){
-					fprintf(stderr,"Error al escribir el archivo de salida\n");
-					exit(1);
-				}
-			}else printf("%s",string);
+	while (caracter != EOF) {		
+		if (encoderActivo) {
+			char string[3];
+			encoder (string, caracter);
+			if (fputs(string, foutput) == EOF) escribir_error();
 		
-		}
-		else{
-			
-			caracter2 = fgetc(finput);
-			
-			if( ferror(finput) ){
-				fprintf(stderr,"Error al leer el archivo de entrada\n");
-				exit(1);
-			}
-			
+		} else {
+			caracter2 = leer(finput);
 			c = decoder(caracter,caracter2);
-			if(foutput != NULL){
-				if( fputc(c,foutput) == EOF ){
-					fprintf(stderr,"Error al escribir el archivo de salida\n");
-					exit(1);
-				}
-			}else printf("%c",c);
-			
-		
+			if (fputc(c, foutput) == EOF) escribir_error();
 		}
-		caracter = fgetc(finput);		
-	}       
-	 
-    if( ferror(finput) ){
-		  fprintf(stderr,"Error al leer el archivo de entrada\n");
-		  exit(1);
-    }
-    fclose(finput);
-    if(foutput != NULL) fclose(foutput);
-}
-
-void EntradaEncoderStandar(){
-	
-	
-	int c = getchar();
-	
-	while (c != EOF) {
-		char* string = encoder(c);
-		if( foutput != NULL ) fprintf(foutput,"%s",string);
-		else printf("%s",string);		
-		c = getchar();		
+		caracter = leer(finput);
 	}
+	
+	if (finput != stdin) fclose(finput);
+	if (foutput != stdout) fclose(foutput);
 }
 
-void EntradaDecoderStandar(){
-	
-	
-	int c = getchar();
-	int c2 = getchar();
-	
-	while (c != EOF && c2 != EOF ) {
-		char string = decoder(c,c2);
-		if( foutput != NULL ){
-			if( fputc(string,foutput) == EOF ) fprintf(stderr,"Error al escribir el archivo de salida\n");
-		
-		} else printf("%c",string);
-		c = getchar();
-		if(c == '\n') c = getchar();
-		c2 = getchar();
-	}	
-
-}
-
-
-void comprobarAction(char* optarg){
-	if( strcmp ( optarg, "encode" ) == 0 ){
+void comprobarAction (char* optarg) {
+	if ( strcmp (optarg, "encode") == 0 ) {
 		encoderActivo = true;
-		decoderActivo = false;
 	}
-	if ( strcmp ( optarg, "decode" ) == 0 ){
+	if ( strcmp (optarg, "decode") == 0 ) {
 		encoderActivo = false;
-		decoderActivo = true;
 	}
 	
 }
 
-void imprimirAyuda(){
+void imprimirAyuda () {
 	printf("Usage:\n");
 	printf("\t ./tp0 -h\n");
 	printf("\t ./tp0 -v\n");
@@ -180,74 +120,56 @@ void imprimirAyuda(){
 	printf("\t ./tp0 -a encode -i /input -o /output -h\n");
 	printf("\t ./tp0 -a decode\n");
 }
-int opciones( int argc , char** argv ){
+
+int opciones (int argc , char** argv, FILE** finput, FILE** foutput) {
 	
 	int option_index = 0;
 	int option = getopt_long ( argc, argv, "vhi:o:a:", long_options, &option_index);
-	while ( option != -1 ){
+	while ( option != -1 ) {
 		
-		switch ( option ){
-			case 'v': 
+		switch (option) {
+			case 'v':
 					printf("66.20-Organizacion de Computadoras TP Version 0.0\n");
 					return 1;
 					break;
 			case 'h':
 					imprimirAyuda();
 					return 1;
-					break;			
-			
-			case 'i':	
-					finput = fopen(optarg,"r");
-					if(finput == NULL ){
+					break;
+			case 'i':
+					(*finput) = fopen(optarg,"r");
+					if ((*finput) == NULL) {
 						fprintf(stderr,"Error al abrir el archivo input %s\n",optarg);
 						exit(1);
 				}
 				break;
 			case 'o':
-					foutput = fopen(optarg, "w");
-					if (foutput == NULL){
+					(*foutput) = fopen(optarg, "w");
+					if ((*foutput) == NULL) {
 						fprintf(stderr,"Error al abrir el archivo output %s \n",optarg);
 						exit(1);
-					}						
+					}
 						break;
-			case 'a':	
+			case 'a':
 					comprobarAction(optarg);
 					break;
-			
 			default:
 					break;
-		}  
+		}
 		
-				
-		option = getopt_long ( argc, argv, "vhi:o:a:", long_options, &option_index);      
-     
-    }	
-   
+		option = getopt_long ( argc, argv, "vhi:o:a:", long_options, &option_index);
+    }
+    
 	return 0;
 }
 
-
-void controlarOpciones(){
+int main (int argc, char** argv) {
+	FILE* finput = stdin;
+	FILE* foutput = stdout;
+	int opcion = opciones (argc, argv, &finput, &foutput);
 	
-	 if (finput != NULL){		
-		procesarArchivos(finput,foutput);		
-	}
-	else{
-		if(encoderActivo) EntradaEncoderStandar();
-		else EntradaDecoderStandar();
-	}
-
-}
-
-int main (int argc, char** argv){
-	int opcion = opciones( argc , argv);
-	
-	if(opcion == 0){
-		controlarOpciones();	
-		//printf("\nSe completo con exito la operacion\n");
-	}
+	if (opcion == 0) procesarArchivos (finput, foutput);
 	return 0;
-
 }
 
 
